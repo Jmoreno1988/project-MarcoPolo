b40f35f1e2c36a5455fac1ca68b3c6c9a0e6fe65
Jmoreno1988 <josemorenocasero@hotmail.com>
2017-01-24 18:00:24 +0100

    Added geoPosition

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ngCordova'])
   
 .controller('cameraTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -48,11 +48,13 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('menuGameCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuGameCtrl', ['$scope', '$stateParams', '$cordovaGeolocation', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-    var map = new ol.Map({
+function ($scope, $stateParams, $cordovaGeolocation) {
+  //ionic.Platform.ready(function(){
+
+var map = new ol.Map({
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
@@ -68,9 +70,29 @@ function ($scope, $stateParams) {
 
 
 
+
+      
+
+
+
+
+        // Code goes here
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+ 
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+         
+
+
+
       var marker = new ol.Feature({
         type: 'icon',
-        geometry: new ol.geom.Point( ol.proj.transform([-3.6819, 40.4145], 'EPSG:4326','EPSG:3857'))
+        geometry: new ol.geom.Point( ol.proj.transform([long, lat], 'EPSG:4326','EPSG:3857'))
       });
 
       var vectorLayer = new ol.layer.Vector({
@@ -84,8 +106,17 @@ function ($scope, $stateParams) {
           })
         })
       });
-
+      console.log(map)
+      map.getView().setCenter(ol.proj.transform([long, lat], 'EPSG:4326','EPSG:3857'));
       map.addLayer(vectorLayer);
+        }, function(err) {
+            console.log(err);
+        });
+    //});
+  /*
+    
+*/
+
 
 }])
    
